e1cc6533bc30dc828b46d21d741caee32f347e4c
Branden <b.lowe333@gmail.com>
2018-06-29 14:31:25 -0700

    renamed files - setup server

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+
+const blogRoutes = require('./scripts/routes/post')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+
+app.use('/blog-posts', blogRoutes)
+
+app.use((req, res, next) => {
+  next({ status: 404, message: 'Route not found - 404' })
+})
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({ error: err })
+})
+
+app.listen(port, () => console.log(`running on port ${port}`))
